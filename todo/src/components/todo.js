331ccf8aec4ee6f78ae598d086ae3c47329341a0
git-austinmccollom <austinmccollom@gmail.com>
2020-09-08 00:14:53 -0500

    strikethrough toggle

--- todo/src/components/todo.js
+++ todo/src/components/todo.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import '../App.css';
 
 export default function Todo(props) {
     const { dispatch, todo } = props;
 
     return(
-        <li onClick={ () => dispatch({ type: 'TOGGLE_COMPLETE', payload: todo.id })}>
+        <li className={ todo.completed === true? "complete" : null } onClick={ () => dispatch({ type: 'TOGGLE_COMPLETE', payload: todo.id })}>
             {
                 todo.item
             }
